86800172512ea2224d32641532e6b9c6e2b81488
sincosvlad <plussincos@gmail.com>
2022-06-30 14:18:08 +0300

    Add resource for offers

--- app/Http/Controllers/API/OfferController.php
+++ app/Http/Controllers/API/OfferController.php
@@ -3,23 +3,25 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Requests\OfferRequest;
+use App\Http\Resources\OfferResource;
 use App\Models\FeedBack;
 
 class OfferController
 {
     public function createOffer(OfferRequest $request)
     {
-        $message = 'New offer from ' . $request->get('userId') . '. ' . $request->get('price') . ' NEAR for ' . $request->get('productName');
+        $message = 'New offer from ' . $request->get('userId') . ': ' . $request->get('price') . ' NEAR for ' . $request->get('productName');
 
         $offer = FeedBack::create([
             'form' => 'make-offer',
             'status' => 'offer',
             'first_name' => $request->get('userId'),
+            'subject' => $request->get('productName'),
             'message' => $message,
             'additional_1' => $request->get('productId'),
             'additional_2' => $request->get('price'),
         ]);
 
-        return $offer;
+        return new OfferResource($offer);
     }
 }
